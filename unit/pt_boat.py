3d2f6a5be33d4e327f2c66fd6616ca516a2c6d2f
David Skrundz <david.skrundz@gmail.com>
2015-02-24 10:33:23 -0700

    Done part 1

---
+++ unit/pt_boat.py
@@ -0,0 +1,45 @@
+from unit.water_unit import WaterUnit
+import unit, helper, effects
+from tiles import Tile
+import pygame
+
+class PTBoat(WaterUnit):
+	"""
+	The PT boat is a faster, smaller, more fragile boat.
+	It does slightly less damage, but can get around more easily.
+
+	Armour: LOW
+	Speed: VERY HIGH
+	Range: MEDIUM
+	Damage: LOW
+
+	Other notes:
+	- Despite its low stats, this unit is constrained to the water, so its
+	  uses are fairly specialized and limited.
+	"""
+	sprite = pygame.image.load("assets/PT-boat.png")
+
+	def __init__(self, **keywords):
+		#load the image for the base class.
+		self._base_image = PTBoat.sprite
+
+		#load the base class
+		super().__init__(**keywords)
+
+		#sounds
+		self.hit_sound = "ArtilleryFire"
+
+		#set unit specific things.
+		self.type = "Battleship"
+		self.speed = 12
+		self.max_atk_range = 6
+		self.damage = 4
+		self.defense = 1
+		self.hit_effect = effects.Explosion
+
+		self.health = 6
+		self.max_health = self.health
+
+		self._update_image()
+
+unit.unit_types["PTBoat"] = PTBoat
--- unit/water_unit.py
+++ unit/water_unit.py
@@ -4,36 +4,36 @@ from tiles import Tile
 import pygame
 
 class WaterUnit(BaseUnit):
-    """
-    A unit which moves through water rather than land.
-    
-    - Only collides with other water units.
-    """
-    def __init__(self, **keywords):
-        #load the base class
-        super().__init__(**keywords)
+	"""
+	A unit which moves through water rather than land.
 
-        #set unit specific things.
-        self.type = "Water Unit"
-        
-        #All water units have the same movement sound
-        self.move_sound = "BoatMove"
-        
-    def is_passable(self, tile, pos):
-        """
-        Returns whether or not this unit can move over a certain tile.
-        """
-        # Return default
-        if not super().is_passable(tile, pos):
-            return False
-                    
-        # We can't pass through enemy units.
-        u = BaseUnit.get_unit_at_pos(pos)
-        if u and u.team != self.team and isinstance(u, WaterUnit):
-            return False
-        
-        #ground units can't travel over water.
-        if (tile.type != 'water'):
-            return False
+	- Only collides with other water units.
+	"""
+	def __init__(self, **keywords):
+		#load the base class
+		super().__init__(**keywords)
 
-        return True
+		#set unit specific things.
+		self.type = "Water Unit"
+
+		#All water units have the same movement sound
+		self.move_sound = "BoatMove"
+
+	def is_passable(self, tile, pos):
+		"""
+		Returns whether or not this unit can move over a certain tile.
+		"""
+		# Return default
+		if not super().is_passable(tile, pos):
+			return False
+
+		# We can't pass through enemy units.
+		u = BaseUnit.get_unit_at_pos(pos)
+		if u and u.team != self.team and isinstance(u, WaterUnit):
+			return False
+
+		#ground units can't travel over water.
+		if (tile.type != 'water'):
+			return False
+
+		return True
